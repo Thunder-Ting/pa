3f2f28a3f71966c50dfc9453090ac062bba3b5b4
tracer-ics2025 <tracer@njuics.org>
2026-01-21 02:04:06 +0800

    >  compile NEMU 231220000 张三 Linux ubuntu 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:04:06 up  8:21,  2 users,  load average: 0.10, 0.05, 0.07

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -16,12 +16,16 @@
 #include "local-include/reg.h"
 #include "debug.h"
 #include <isa.h>
-
+#include <stdio.h>
 const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
                       "s0", "s1", "a0",  "a1",  "a2", "a3", "a4", "a5",
                       "a6", "a7", "s2",  "s3",  "s4", "s5", "s6", "s7",
                       "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
-void isa_reg_display() { Log("TODO:打印所有寄存器的值"); }
+void isa_reg_display() {
+  for (int i = 0; i < 32; i++) {
+    printf("%s\t%x\t", reg_name(i), gpr(i));
+  }
+}
 
 word_t isa_reg_str2val(const char *s, bool *success) { return 0; }
